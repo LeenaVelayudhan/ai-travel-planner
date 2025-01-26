5c612301d6b0565909b36b98e4da7396edc13c7f
LeenaVelayudhan <leenavelayudhan93@gmail.com>
2025-01-27 03:42:08 +0530

    Landing page modified

--- src/create-trip/custom/Hero.jsx
+++ src/create-trip/custom/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Button } from "../../components/ui/button"; // Adjust path as necessary
+import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="flex flex-col items-center mx-57 gap-9">
+      <h1
+      className="font-extrabold text-[50px] text-center mt-16">
+        <span className='text-[#5d04f8]'>Discover Your Next Adventure with AI:</span> <br></br> Personalized Itineraries at Your Fingertips</h1>
+        <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator,creating custom itineraries tailored to your interests and budget.</p>
+        <Link to={'/create-trip'}>
+          <Button>Get Started, It's Free.</Button>
+        </Link>    </div>
   )
 }
 
