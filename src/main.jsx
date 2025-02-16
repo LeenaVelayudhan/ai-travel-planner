7b424b134d71d9f5a395b61f85e3016b53a6ba75
LeenaVelayudhan <leenavelayudhan93@gmail.com>
2025-02-16 12:05:37 +0530

    displaying trip plan

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import CreateTrip from './create-trip';
 import Header from './create-trip/custom/Header';
 import { Toaster } from './components/ui/sonner';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Viewtrip from './view-trip/[tripId]/index.jsx';
 // Define routes
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
     path:'/create-trip',
     element:<
     CreateTrip/>
+  },
+  {
+    path:'view-trip/:tripId',
+    element:<Viewtrip/>
   }
 ]);
 
